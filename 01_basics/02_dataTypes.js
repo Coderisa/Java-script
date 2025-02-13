2217e0027d9da35724a05941f5a4f1a0eb32b2e2
Ina Sharma <142999200+Coderisa@users.noreply.github.com>
2025-02-13 14:31:55 +0530

    Create 02_dataTypes.js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict";//treat all JS code as newer version
+
+// alert(3+3) //we r usong nodejs not browser
+
+console.log(3+3) ;console.log("ina") //code readibility should be high
+//Documentation -MDN ,TC39
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean =>true/false
+//null => standalone value
+//undefined=> 
+//symbol=>unique
+
+
+console.log(typeof undefined);//undefined
+console.log(typeof null);// object
