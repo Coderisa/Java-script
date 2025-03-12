f3a714e00dd364e6296c56334f9974c9125587c7
Ina Sharma <142999200+Coderisa@users.noreply.github.com>
2025-03-13 02:58:28 +0530

    Update 02_dataTypes.js

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,18 +1,25 @@
 "use strict";//treat all JS code as newer version
 
-// alert(3+3) //we r usong nodejs not browser
+// alert(3+3) //we r usong nodejs not browser to nhi chal rha
 
 console.log(3+3) ;console.log("ina") //code readibility should be high
 //Documentation -MDN ,TC39
 
 //number => 2 to power 53
 //bigint
-//string => ""
+//string => "" or ''
 //boolean =>true/false
-//null => standalone value
-//undefined=> 
+//null => standalone value, let user = null; // No user yet, intentionally left empty.
+//undefined=> let age;
 //symbol=>unique
 
-
+//object
 console.log(typeof undefined);//undefined
 console.log(typeof null);// object
+/*
+Feature	null	undefined
+Type	Object	Undefined
+Assigned by	Developers (explicitly)	JavaScript (implicitly)
+Purpose	Intentional absence of value	Lack of assignment (default)
+Usage	Clear or reset a variable	Uninitialized or missing values
+*/
